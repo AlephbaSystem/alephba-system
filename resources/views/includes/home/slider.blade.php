1a25dce94a237eef7ea042a990ff80abb7cce7f6
Soroush Torabi <tspersian@hotmail.com>
2021-10-30 08:57:44 +0330

    init

---
+++ resources/views/includes/home/slider.blade.php
@@ -0,0 +1,41 @@
+<div id="slider" class="inspiro-slider slider-halfscreen dots-creative" data-height-xs="360" data-animate-in="fadeIn" data-animate-out="fadeOut" data-items="1" data-loop="true" data-autoplay="true">
+    <!-- Slide 1 -->
+    <div class="slide" data-bg-image="/homepages/construction/images/1.webp" style="object-fit: cover">
+        <div class="container">
+            <div class="slide-captions {!! __('sRTL1') !!}">
+                <!-- Captions -->
+                <h2 class="text-medium m-b-50">
+                    <span style="background-color: #26AB5B; padding: 10px" class="text-light">{!! __("HS1_Title") !!}</span>
+                </h2>
+                <p class="lead text-dark">
+                    {!! __("HS1_Details") !!}
+                </p>
+                <a class="btn btn-dark" href="/Services">{!!
+                    __("More")
+                    !!}</a>
+
+                <!-- end: Captions -->
+            </div>
+        </div>
+    </div>
+    <!-- end: Slide 1 -->
+    <!-- Slide 2 -->
+    <div class="slide" data-bg-image="/homepages/construction/images/2.webp">
+        <div class="container">
+            <div class="slide-captions {!! __('sRTL2') !!}">
+                <!-- Captions -->
+                <h2 class="text-medium m-b-50">
+                    <span style="background-color: #26AB5B; padding: 10px" class="text-light">{!! __("HS2_Title") !!}</span>
+                </h2>
+                <p class="lead text-dark">
+                    {!! __("HS2_Details") !!}
+                </p>
+                <a class="btn btn-dark" href="/Services">{!!
+                    __("More")
+                    !!}</a>
+                <!-- end: Captions -->
+            </div>
+        </div>
+    </div>
+    <!-- end: Slide 2 -->
+</div>
