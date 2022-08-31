403ae47bc963e5a51bfa91360e3ce466ae3fd6ef
Soroush Trb <tspersian@gmail.com>
2022-08-31 16:22:25 +0430

    add matomo

--- resources/views/includes/global/head.blade.php
+++ resources/views/includes/global/head.blade.php
@@ -12,3 +12,22 @@
 <link href="css/style.css" rel="stylesheet">
 {!! __("RTLCSS") !!}
 {!! __("RTLFont") !!}
+<!-- Matomo -->
+<script>
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u = "//wapi.alephba-system.ir/";
+        _paq.push(['setTrackerUrl', u + 'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d = document,
+            g = d.createElement('script'),
+            s = d.getElementsByTagName('script')[0];
+        g.async = true;
+        g.src = u + 'matomo.js';
+        s.parentNode.insertBefore(g, s);
+    })();
+</script>
+<!-- End Matomo Code -->
